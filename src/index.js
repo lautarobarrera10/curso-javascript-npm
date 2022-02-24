69b313b68f4edbcef0eda9a18824ec08a04b0bea
Lautaro <lautarobarrera12@gmail.com>
2022-02-24 16:56:36 -0300

    Aprendiendo a instalar paquetes

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// NOTAS
+
+// INSTALANDO PAQUETES
+
+// PROYECTO
+// Producción:
+// npm install [paquete] --save
+// npm i [paquete] -D
+
+// Desarrollo:
+// npm install [paquete] --save-dev
+// npm i [paquete] -D
+
+// Si no ponemos nada después del paquete por defecto se guarda para producción
+
+// GLOBAL
+// Para instalar un paquete de manera global (en caso de error usar sudo)
+// npm install -g [paquete]
+// Para saber los recursos que están instalados de forma global
+// npm list -g --depth 0
